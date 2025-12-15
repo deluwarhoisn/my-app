157742942509dfec37c56d19191c25ae2531c14d
deluwarhoisn <babuhossen301@gmail.com>
2025-12-15 22:53:29 +0600

    add all section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,17 @@
-import Banner from "@/componets/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-  <Banner>
-    
-  </Banner>
+    <div className="flex flex-col gap-5 min-h-screen justify-center text-center items-stretch">
+      <div className="relative ">
+        <h2 className="text-4xl font-bold">Next JS Fetching & Rendering</h2>
+        <h2 className="text-8xl absolute inset-0 -top-20  opacity-10 font-medium">
+          Fetching Rendering & Styling
+        </h2>
+      </div>
+      <div className="">
+        <button className="btn">Lets Explore</button>
+      </div>
     </div>
   );
 }
